9d9a020da228196abe8d11a7045e8cef7b60f5fe
AlonsoLJ <96770551+AlonsoLJ@users.noreply.github.com>
2022-01-06 18:50:30 +0100

    Update Monitor.hpp

--- Streaming&Analizadores/Monitor.hpp
+++ Streaming&Analizadores/Monitor.hpp
@@ -7,15 +7,10 @@ const int NUM_MAXIMOS= 10000;
 class Control{  
     public:
         Control();   //constructor
-        void entraTags();
-        void saleTags();
-        void entraQos();
-        void saleQos();
-
+        void entra();
+        void sale();
     private:
-        bool puedoEntrarTags;
-        bool puedoEntrarQos;
+        bool puedoEntrar;
         mutex mtxMonitor; //FUNDAMENTAL: mutex usarán las funcs
-        condition_variable esperandoTags;     
-        condition_variable esperandoQos;  
+        condition_variable esperando;      
 };
